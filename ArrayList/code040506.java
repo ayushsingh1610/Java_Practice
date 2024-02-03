93bd40d798c4ad6bd5dd7d670c08ef142823abc1
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-03 22:40:08 +0530

    Add code04 05 06 files in a single file

---
+++ ArrayList/code040506.java
@@ -0,0 +1,67 @@
+// Write a Java program to update an array element by the given element.
+// Write a Java program to remove the third element from an array list.
+// Write a Java program to search for an element in an array list.
+import java.util.*;
+public class code040506 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> ob = new ArrayList<>();
+        Input(ob);
+
+        System.out.print("Enter your choice = ");
+        int ch = sc.nextInt();
+        switch(ch)
+        {
+            case 1:
+            {
+                Scanner s = new Scanner(System.in);
+
+                System.out.print("Enter the new element= ");
+                String element = s.nextLine();
+
+                System.out.print("Enter the position = ");
+                int n = s.nextInt();
+
+                ob.set(n-1, element);
+
+                break;
+            }
+            case 2:
+            {
+                ob.remove(2);
+
+                break;
+            }
+
+            case 3:
+            {
+                Scanner s = new Scanner(System.in);
+                System.out.print("Enter the element to find = ");
+                String str = s.nextLine();
+
+                if(ob.contains(str))    System.out.println("Element present");
+                else    System.out.println("Element Not Present");
+
+                break;
+            }
+        }
+        
+        Display(ob);
+    }
+    public static void Input(ArrayList<String> a)
+    {
+        a.add("Yellow");
+        a.add("Red");
+        a.add("Blue");
+        a.add("Green");
+        a.add("Purple");
+    }
+    public static void Display(ArrayList<String> a)
+    {
+        Iterator it = a.iterator(); 
+            while(it.hasNext())
+            {
+                System.out.print(it.next() + "  ");
+            }
+    }
+}
