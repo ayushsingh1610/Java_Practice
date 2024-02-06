056e65e3bdc03eef16b6a031680c4cbe67819283
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-06 23:02:43 +0530

    Update README

---
+++ Hashing/README.md
@@ -0,0 +1,2 @@
+Write a Java program to compare two hash set.
+Write a Java program to associate the specified value with the specified key in a HashMap.
--- Hashing/code01.java
+++ Hashing/code01.java
@@ -1,10 +1,21 @@
-// 
+// Write a Java program to compare two hash set.
 import java.util.*;
 class code01
 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         HashSet<Integer> ob = new HashSet<>();
-        System.out.print("");
+        HashSet<Integer> ob2 = new HashSet<>();
+        Input(ob);
+        Input(ob2);
+
+    }
+    public static void Input(HashSet<Integer> hs) {
+        hs.add(2);
+        hs.add(4);
+        hs.add(6);
+        hs.add(3);
+        hs.add(9);
+        hs.add(2);
     }
 }
