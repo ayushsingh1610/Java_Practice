d2c37f73fbc576f6283a611aeb99e4dfae847b42
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-06 21:55:21 +0530

    Add code01

---
+++ Hashing/code01.java
@@ -0,0 +1,10 @@
+// 
+import java.util.*;
+class code01
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> ob = new HashSet<>();
+        System.out.print("");
+    }
+}
