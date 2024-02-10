d4b57601301d44847ec301b2c3b7d9b870727619
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-10 21:38:20 +0530

    Add code08

---
+++ IO Stream/code08.java
@@ -0,0 +1,13 @@
+// Write a Java program to get the file size in bytes, KB, MB.
+import java.io.*;
+public class code08 {
+    public static void main(String[] args)throws IOException {
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        File f = new File("C:\\Users\\good\\Documents\\Temporary\\b.txt");
+
+        
+        System.out.println("Size of file is = " + f.length());
+        
+    }
+}
