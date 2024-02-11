0c1aa5ccfe050d1683f74f3fc40062498205a08e
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-11 16:35:56 +0530

    Update code03

--- Exception-Handling/code03.java
+++ Exception-Handling/code03.java
@@ -1,9 +1,7 @@
 // Write a Java program to create a method that reads a file and throws an exception if the file is not found.
 import java.io.*;
-import java.util.*;
 public class code03 {
     public static void main(String[] args)throws FileNotFoundException {
-        Scanner sc = new Scanner (System.in);
         File f = new File("C:\\Users\\good\\Documents\\Temporary\\a.txt");
             if(f.canRead())
             {
