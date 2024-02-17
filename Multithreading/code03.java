60d2c3f598626fa193dfaf06038274eee52d9ad2
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-17 12:22:41 +0530

    Update code03

--- Multithreading/code03.java
+++ Multithreading/code03.java
@@ -1,21 +1,80 @@
 // Write a Java program that sorts an array of integers using multiple threads.
 import java.util.*;
-public class code03 extends Thread
+
+class Bubble_Sort extends Thread
+{
+    int a[] = {2,9,7,3,4,1};
+    public void run()
+    {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i+1; j < a.length; j++) {
+                if(a[i]>a[j])
+                {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
+class Selection_Sort extends Thread
+{
+    int a[] = {2,9,7,3,4,1};
+    public void run()
+    {
+        int index = 0;
+        for (int i = 0; i < a.length; i++) {
+            index = i;
+            for (int j = i+1; j < a.length; j++) {
+                if(a[index]>a[j])   index = j;
+            }
+            int temp = a[index];
+            a[index] = a[i];
+            a[i] = temp;
+        }
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
+class Insertion_Sort extends Thread
+{
+    int a[] = {2,9,7,3,4,1};
+    public void run()
+    {
+        for (int i = 1; i < a.length; i++) {
+            int key = a[i];
+            int j=i-1;
+
+            while(j>=0 && a[j]>key)
+            {
+                a[j+1] = a[j];
+                j--;
+            }
+            a[j+1] = key;
+            
+        }
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
+public class code03 
 {
     public static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
-        List<Integer> a = new ArrayList<>();
-        a.add(2);
-        a.add(9);
-        a.add(7);
-        a.add(1);
-        code03 ob = new code03();
-        ob.run(a);
-        System.out.println(a);
-    }
-    void run(List<Integer> a)
-    {
-        Collections.sort(a);
+        Bubble_Sort t1 = new Bubble_Sort();
+        Selection_Sort t2 = new Selection_Sort();
+        Insertion_Sort t3 = new Insertion_Sort();
+        t1.start();
+        // t2.start();
+        // t3.start();
     }
 }
