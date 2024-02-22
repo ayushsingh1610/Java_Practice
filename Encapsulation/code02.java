75d8757eb45605f36320a1fe7ddca3042fc29164
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-02-22 23:59:01 +0530

    Create code02

---
+++ Encapsulation/code02.java
@@ -0,0 +1,14 @@
+// Write a Java program to create a class called BankAccount with private instance variables accountNumber and balance. Provide public getter and setter methods to access and modify these variables.
+import java.util.*;
+class BankAccount
+{
+    private int accountNumber;
+    private double balance;
+}
+public class code02
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+    }
+}
