ce35fe0980189cc0aa86e426468e00bb30b14bef
AYUSH SINGH <ayushsingh1610@outlook.com>
2024-03-13 23:21:20 +0530

    Add code01

---
+++ Bit-Manipulation/code01.java
@@ -0,0 +1,13 @@
+// Check if an integer is even or odd.
+class code01
+{
+    public static void main(String[] args) {
+        int a = 10;
+
+        if((a&1)==0)
+        System.out.println(a + " is Even");
+        
+        else
+        System.out.println(a + " is ODD");
+    }
+}
